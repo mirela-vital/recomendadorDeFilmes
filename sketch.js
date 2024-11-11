a246cd6fe7a57c8b6516b340b4ed3955fc8ff23d
mirela-vital <156128054+mirela-vital@users.noreply.github.com>
2024-11-11 17:51:19 -0300

    Atualização do sketch.js

--- sketch.js
+++ sketch.js
@@ -1,27 +1,52 @@
-let campoIdade;
-let campoFantasia;
-let campoAventura;
+// fantasia, aventura, drama
 
+// a viagem de chihiro, LIVRE, fantasia, aventura
+// paddington, LIVRE, fantasia, aventura
+
+// as aventuras de pi, 10, drama, fantasia, aventura
+// guardiões da galáxia, 12, fantasia, aventura
+// ladrões de bicicleta, 12, drama
+// o menino que descobriu o vento, 14, drama
+
+let campoIdade;
 
 function setup() {
-  createCanvas(800, 400);
-  createElement("h2", "Recomendador de filmes");
-  createSpan("Sua idade:");
-  campoIdade = createInput("5");
-  campoFantasia = createCheckbox("Gosta de fantasia?");
-  campoAventura = createCheckbox("Gosta de aventura?");
+  createCanvas(600, 400);
+  campoIdade = createInput("15");
+  campoFantasias = createCheckbox("Gosta de fantasia?");
+  
 }
 
 function draw() {
-  background("white");
+  background("#95BEDF");
+  let gostaDeFantasias =campoFantasias.checked();
   let idade = campoIdade.value();
-  let gostaDeFantasia = campoFantasia.checked();
-  let gostaDeAventura = campoAventura.checked();
-  let recomendacao =  geraRecomendacao(idade, gostaDeFantasia, gostaDeAventura);
-
-  fill(color(76, 0, 115));
+  let recomendacao = geraRecomendacao(idade,gostaDeFantasias);
+  fill(color(75,0,120));
   textAlign(CENTER, CENTER);
   textSize(38);
   text(recomendacao, width / 2, height / 2);
 }
 
+function geraRecomendacao(idade,gostaDeFantasias) {
+  if (idade >= 10) {
+    if (idade >= 14 && idade <= 40) {
+      return "O menino que descobriu o vento";
+    } else {
+      if (idade > 40) {
+        return "sexta-feira 13";
+      } else {
+        if (gostaDeFantasias) {
+          return "As aventuras de Pi";
+        }else{
+          return "Depois da Chuva";
+        }
+      }
+    }
+  } else {
+    if(gostaDeFantasias){
+      return "A viagem de Chihiro";
+    }
+    return "O feitiço do tempo";
+  }
+}
