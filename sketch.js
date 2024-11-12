11380b5452c75aeba21caeeddf1208f32b82fdca
mirela-vital <156128054+mirela-vital@users.noreply.github.com>
2024-11-12 16:20:12 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -18,35 +18,43 @@ function setup() {
 }
 
 function draw() {
-  background("#95BEDF");
-  let gostaDeFantasias =campoFantasias.checked();
+  background("white");
   let idade = campoIdade.value();
-  let recomendacao = geraRecomendacao(idade,gostaDeFantasias);
-  fill(color(75,0,120));
+  let gostaDeFantasia = campoFantasia.checked();
+  let gostaDeAventura = campoAventura.checked();
+  let recomendacao = geraRecomendacao(idade, gostaDeFantasia, gostaDeAventura);
+
+  fill(color(76, 0, 115));
   textAlign(CENTER, CENTER);
   textSize(38);
   text(recomendacao, width / 2, height / 2);
 }
 
-function geraRecomendacao(idade,gostaDeFantasias) {
+function geraRecomendacao(idade, gostaDeFantasia, gostaDeAventura) {
   if (idade >= 10) {
-    if (idade >= 14 && idade <= 40) {
+    if (idade >= 14) {
       return "O menino que descobriu o vento";
     } else {
-      if (idade > 40) {
-        return "sexta-feira 13";
+      if (idade >= 12) {
+        if(gostaDeFantasia || gostaDeAventura) {
+          return "Homem aranha: no aranhaverso";          
+        } else{
+         return "Ladrões de bicicleta";
+        }
       } else {
-        if (gostaDeFantasias) {
-          return "As aventuras de Pi";
-        }else{
-          return "Depois da Chuva";
+        if (gostaDeFantasia) {
+          return "As aventuras de pi";
+        } else {
+          return "Depois da chuva";
         }
       }
     }
   } else {
-    if(gostaDeFantasias){
-      return "A viagem de Chihiro";
+    if (gostaDeFantasia) {
+      return "A viagem de chihiro";
+    } else {
+      return "O feitiço do tempo";
     }
-    return "O feitiço do tempo";
   }
 }
+
